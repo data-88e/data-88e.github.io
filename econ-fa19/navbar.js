b4326a544771d625ab33abf78abacab9c22462fa
Chris Pyles <cpyles@berkeley.edu>
2019-09-03 22:59:52 -0700

    updating to bootstrap 4

--- econ-fa19/navbar.js
+++ econ-fa19/navbar.js
@@ -1,57 +1,57 @@
 var html = `
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
+<nav class="navbar navbar-light bg-light navbar-expand-lg sticky-tp[">
+    <!-- <div class="container"> -->
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
         <a class="navbar-brand" href="./">
           <img src="assets/images/blue_text.png" height="25px" />
         </a>
 
+        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarLinks" aria-controls="navbarLinks" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
       </div>
-      <div id="navbar" class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
 
-        	<li><a href=".">EconModels</a></li>
-        	<li><a>|</a></li>
-            <!-- <li><a href="./faq.html">FAQ</a></li> -->
-            <li><a href="./policies.html">Policies</a></li>
-            <!-- <li><a href="./staff.html">Staff</a></li> -->
-            <li><a href="https://piazza.com/berkeley/fall2019/data88" target="_blank">Piazza</a></li>
-            <li><a href="https://www.gradescope.com/courses/56798" target="_blank">Gradescope</a></li>
-            <li><a href="attendance.html">Attendance</a></li>
+      <div class="collapse navbar-collapse" id="navbarLinks">
 
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Resources <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-                  <!-- <li><a href="./materials.html">Materials</a></li> -->
-                  <li><a href="./python-reference.html">Python Reference</a></li>
-                  <li><a href="http://data8.org/datascience/" target="_blank">Datascience Docs</a></li>
+            <ul class="navbar-nav mr-auto justify-content-end">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">EconModels</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">|</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="policies.html">Policies</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="https://piazza.com/berkeley/fall2019/data88">Piazza</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.gradescope.com/courses/56798">Gradescope</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="attendance.html">Attendance</a>
+                </li>
+
+                <li class="dropdown nav-item">
+                  <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Resources <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                        <!-- <li><a href="./materials.html">Materials</a></li> -->
+                        <li><a href="./python-reference.html">Python Reference</a></li>
+                        <li><a href="http://data8.org/datascience/" target="_blank">Datascience Docs</a></li>
+                  </ul>
+                </li>
+
             </ul>
-          </li>
 
-          <!--
-
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Links <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-                  <li><a href="http://data8.org/connector/">Connectors</a></li>
-                  <li><a href="http://summer.datahub.berkeley.edu/">DataHub</a></li>
-                  <li><a href="http://data8.org/datascience/">Datascience Docs</a></li>
-                  <li><a href="https://piazza.com/berkeley/fall2019/data88">Piazza</a></li>
-                  <li><a href="http://data.berkeley.edu">Berkeley Division of Data Sciences</a></li>
-            </ul>
-          </li>
-
-          -->
-
-        </ul>
-      </div><!--/.nav-collapse -->
-    </div>
+        </div>
   </nav>
   `;
 
